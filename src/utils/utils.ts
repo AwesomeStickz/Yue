abde42102e7014ac9091ed638caf4cb51f1f4e4b
Awesome Stickz <kiran121102@gmail.com>
2020-08-18 11:15:37 +0530

    Return undefined if arg length is less than 1 in utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,7 @@ import { Client, Guild } from 'discord.js';
 
 export const utils = {
     getMember(arg: string, guild: Guild) {
+        if (arg.length < 1) return undefined;
         let member = guild.members.cache.get(arg);
         if (!member) member = guild.members.cache.filter((m) => m.user.username.toLowerCase().includes(arg.toLowerCase())).first();
         if (!member) member = guild.members.cache.filter((m) => m.user.tag.toLowerCase() === arg.toLowerCase()).first();
@@ -11,6 +12,7 @@ export const utils = {
         else return undefined;
     },
     async getUser(arg: string, client: Client) {
+        if (arg.length < 1) return undefined;
         let user = client.users.cache.filter((u) => u.username.toLowerCase().includes(arg.toLowerCase())).first();
         if (!user) user = client.users.cache.filter((u) => u.tag.toLowerCase() === arg.toLowerCase()).first();
         if (!user && /[0-9]{17,20}/.test(arg)) {
