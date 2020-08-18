072370b7b7d6a3630fdc0e50609489ad61940d31
Awesome Stickz <kiran121102@gmail.com>
2020-08-18 14:23:04 +0530

    Added balance set

---
+++ src/commands/balance set.ts
@@ -0,0 +1,36 @@
+import { Client, Message } from 'discord.js';
+import { database } from '../utils/databaseFunctions';
+import { embed } from '../utils/embed';
+import { emojis } from '../utils/emojis';
+import { utils } from '../utils/utils';
+
+export const run = async (message: Message, client: Client, args: string[]): Promise<Message | void> => {
+    const user = await utils.getUser(args[0], client);
+    const balanceEmbed = embed({
+        color: message.guild?.me?.displayHexColor,
+    });
+
+    if (!user) return message.channel.send(balanceEmbed.setDescription(`${emojis.tickNo} I couldn't find that user!`));
+    if (user.bot) return message.channel.send(balanceEmbed.setDescription(`${emojis.tickNo} Bots doesn't have bank accounts!`));
+    if (isNaN(Number(args[1]))) return message.channel.send(balanceEmbed.setDescription(`${emojis.tickNo} Balance amount must be a number!`));
+
+    const balance = Number(args[1]);
+    await database.setProp('economy', user.id, balance, 'balance');
+
+    balanceEmbed.setAuthor(user.username, user.displayAvatarURL());
+    message.channel.send(balanceEmbed.setDescription(`${emojis.tickYes} ${user.toString()}'s balance has been set to **$${balance.toLocaleString()}**`));
+};
+
+export const help = {
+    aliases: ['bal set', 'balance set'],
+    name: 'Balance Set',
+    description: "Set someone's balance",
+    usage: 'balance set <user> <amount>',
+    example: 'balance set @* 500',
+};
+
+export const config = {
+    args: 2,
+    owner: true,
+    subCommand: true,
+};
