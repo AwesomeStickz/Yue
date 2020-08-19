fefd9f05be3fa496220184003d9b36ce70b0854b
Awesome Stickz <kiran121102@gmail.com>
2020-08-19 15:26:11 +0530

    Added paper, rock emoji in emojis.ts

--- src/utils/emojis.ts
+++ src/utils/emojis.ts
@@ -1,4 +1,6 @@
 export const emojis = {
+    paper: '<:Paper:745581111530487848>',
+    rock: '<:Rock:745581111354327072>',
     tickYes: '<:tickYes:744804241926782978>',
     tickNo: '<:tickNo:744804242266783784>',
 } as const;
