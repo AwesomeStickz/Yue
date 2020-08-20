b5f20705984dd481b5cdeb5e86b67e9240248430
Awesome Stickz <kiran121102@gmail.com>
2020-08-20 10:59:08 +0530

    Set commandName as fileName if it's specified in help

--- src/events/message.ts
+++ src/events/message.ts
@@ -22,11 +22,7 @@ export const run = async (client: Client, message: Message): Promise<Message | v
 
         if ((commands.get(command) as any).config.owner === true && !owners.includes(message.author.id)) return;
 
-        if ((commands.get(command) as any).config.args > args.length) return message.channel.send(`Invalid arguments. Correct usage: \`${prefix}${(commands.get(command) as any).help.usage}\``);
-
-        if (!(commands.get(command) as any).config.subCommand === true) command = command.replace(/ /g, '');
-
-        const commandName = command.toLowerCase();
+        const commandName = commandObj.help.fileName ? commandObj.help.fileName : command.toLowerCase();
         const commandFile = require(`../commands/${commandName}`);
 
         commandFile.run(message, client, args);
