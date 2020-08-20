307563975da6c04335c0b0a0372a5399e6c3450b
Awesome Stickz <kiran121102@gmail.com>
2020-08-20 10:59:51 +0530

    Added scissors in emojis

--- src/utils/emojis.ts
+++ src/utils/emojis.ts
@@ -1,6 +1,7 @@
 export const emojis = {
     paper: '<:Paper:745581111530487848>',
     rock: '<:Rock:745581111354327072>',
+    scissors: '✂️',
     tickYes: '<:tickYes:744804241926782978>',
     tickNo: '<:tickNo:744804242266783784>',
 } as const;
