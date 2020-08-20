e88af074a3e992f22f048ecd681143545358b7f3
Awesome Stickz <kiran121102@gmail.com>
2020-08-20 14:52:41 +0530

    Added inventory in PropertyNames in datbaseFunctions

--- src/utils/databaseFunctions.ts
+++ src/utils/databaseFunctions.ts
@@ -2,7 +2,7 @@ import lodash from 'lodash';
 import sequelize from '../database/sequelize';
 
 type DatbaseModelNames = 'cooldown' | 'economy' | 'guildsettings' | 'yue';
-type PropertyNames = 'balance' | 'daily' | 'don' | 'luck' | 'owners' | 'prefix' | 'streak' | 'weekly' | 'winnings';
+type PropertyNames = 'balance' | 'daily' | 'don' | 'inventory' | 'luck' | 'owners' | 'prefix' | 'streak' | 'weekly' | 'winnings';
 
 export const database = {
     async all(model: DatbaseModelNames): Promise<object[]> {
