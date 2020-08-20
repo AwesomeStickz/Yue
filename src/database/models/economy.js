adabae4a60306ccc680bc17af4814a51172692f2
Awesome Stickz <kiran121102@gmail.com>
2020-08-20 14:34:59 +0530

    Added shops, workers in inventory in economy model

--- src/database/models/economy.js
+++ src/database/models/economy.js
@@ -1,4 +1,4 @@
-import { JSONB, NUMBER, STRING } from 'sequelize';
+import { INTEGER, JSONB, NUMBER, STRING } from 'sequelize';
 import { define } from '../sequelize';
 
 export const economy = define('economy', {
@@ -8,6 +8,48 @@ export const economy = define('economy', {
     data: {
         type: JSONB,
         balance: NUMBER,
+        inventory: {
+            shops: {
+                flower: INTEGER,
+                taco: INTEGER,
+                burger: INTEGER,
+                pizza: INTEGER,
+                barber: INTEGER,
+                optician: INTEGER,
+                chemist: INTEGER,
+                butcher: INTEGER,
+                baker: INTEGER,
+                shoe: INTEGER,
+                clothes: INTEGER,
+                book: INTEGER,
+                grocery: INTEGER,
+                diy: INTEGER,
+                toy: INTEGER,
+                music: INTEGER,
+                jewelry: INTEGER,
+                plane: INTEGER,
+            },
+            workers: {
+                flower: INTEGER,
+                taco: INTEGER,
+                burger: INTEGER,
+                pizza: INTEGER,
+                barber: INTEGER,
+                optician: INTEGER,
+                chemist: INTEGER,
+                butcher: INTEGER,
+                baker: INTEGER,
+                shoe: INTEGER,
+                clothes: INTEGER,
+                book: INTEGER,
+                grocery: INTEGER,
+                diy: INTEGER,
+                toy: INTEGER,
+                music: INTEGER,
+                jewelry: INTEGER,
+                plane: INTEGER,
+            }
+        },
         luck: NUMBER,
         streak: NUMBER,
         defaultValue: {},
