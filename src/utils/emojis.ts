52cfe407353882897fb02c48deec6032a5a0c95a
Awesome Stickz <kiran121102@gmail.com>
2020-08-21 13:53:01 +0530

    Added all houses emoji in emojis.ts

--- src/utils/emojis.ts
+++ src/utils/emojis.ts
@@ -1,4 +1,15 @@
 export const emojis = {
+    houses: {
+        apartment: '<:Apartment:744302863391391834>',
+        bungalow: '🏠',
+        caravan: '<:Caravan:746031340830589020>',
+        house: '<:House:744304109888208986>',
+        mansion: '<:Mansion:744302803098533898>',
+        mud: '<:MudHouse:746026271749701722>',
+        penthouse: '<:Penthouse:744302835662979072>',
+        shack: '<:Shack:746027605010022531>',
+        tent: '🎪',
+    },
     paper: '<:Paper:745581111530487848>',
     rock: '<:Rock:745581111354327072>',
     scissors: '✂️',
