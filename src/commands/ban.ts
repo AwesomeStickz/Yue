23198abd1bcd3b92739c431b33d9af853c49e9c9
Awesome Stickz <kiran121102@gmail.com>
2020-08-22 23:56:27 +0530

    Added ban

---
+++ src/commands/ban.ts
@@ -0,0 +1,73 @@
+import { Client, Message } from 'discord.js';
+import { embed } from '../utils/embed';
+import { emojis } from '../utils/emojis';
+import { utils } from '../utils/utils';
+
+export const run = async (message: Message, client: Client, args: string[]): Promise<Message | void> => {
+    const banEmbed = embed({
+        author: {
+            image: message.author.displayAvatarURL(),
+            name: message.author.username,
+        },
+        color: message.guild?.me?.displayHexColor,
+    });
+
+    if (!message.member!.hasPermission('BAN_MEMBERS')) return message.channel.send(banEmbed.setDescription(`${emojis.tickNo} You need **Ban Members** permission to use this command!`));
+    if (!message.guild!.me!.hasPermission('BAN_MEMBERS')) return message.channel.send(banEmbed.setDescription(`${emojis.tickNo} I need **Ban Members** permission to ban someone!`));
+
+    const user = await utils.getUser(args[0], client, message.guild!);
+    if (!user) return message.channel.send(`${emojis.tickNo} I couldn't find that user`);
+
+    const member = utils.getMember(user.id, message.guild!);
+    const reason = args.slice(1).join(' ') || `${message.author.tag} used ban command`;
+
+    if (member) {
+        const userNotOwner = message.guild!.ownerID !== user.id;
+        const authorNotOwner = message.guild!.ownerID !== message.author.id;
+
+        if (authorNotOwner && member.id !== message.author.id && member.roles.highest.rawPosition >= message.member!.roles.highest.rawPosition) return message.channel.send(banEmbed.setDescription(`${emojis.tickNo} Sorry but you’re inferior to the person you’re trying to ban`));
+        if (member.roles.highest.rawPosition >= message.guild!.me!.roles.highest.rawPosition || !userNotOwner) return message.channel.send(banEmbed.setDescription(`${emojis.tickNo} Sorry but I'm inferior to the person you’re trying to ban`));
+    }
+
+    if (user.id === message.author.id) {
+        message.channel.send(banEmbed.setDescription(`${emojis.tickNo} Sorry but you’re too important for me to ban you!`));
+    } else {
+        try {
+            if (member) {
+                const banDMEmbed = embed({
+                    author: {
+                        image: message.author.displayAvatarURL(),
+                        name: `You have been banned from ${message.guild?.name}`,
+                    },
+                    fields: [
+                        { name: 'Moderator', value: message.author.tag },
+                        { name: 'Reason', value: reason === `${message.author.tag} used ban command` ? 'None Given' : reason },
+                    ],
+                    timestamp: true,
+                });
+                await user.send(banDMEmbed);
+            }
+        } catch (_) {}
+
+        await message.guild!.members.ban(user.id, { reason });
+
+        banEmbed.setAuthor(user.username, user.displayAvatarURL());
+        banEmbed.setDescription(`${emojis.tickYes} **${user.tag}** has been banned!`);
+        message.channel.send(banEmbed);
+    }
+};
+
+export const help = {
+    aliases: ['ban'],
+    name: 'Ban',
+    description: 'Ban a user from your server',
+    usage: 'ban <user> [reason]',
+    example: 'ban @Conor#0751',
+};
+
+export const config = {
+    args: 1,
+    botPermissions: ['Ban Members'],
+    userPermissions: ['Ban Members'],
+    module: 'moderation',
+};
