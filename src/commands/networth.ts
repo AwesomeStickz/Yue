9165fa61987a4668dc89282c5f6042caedb63198
Awesome Stickz <kiran121102@gmail.com>
2020-08-22 17:39:58 +0530

    Added networth

---
+++ src/commands/networth.ts
@@ -0,0 +1,35 @@
+import { Client, Message } from 'discord.js';
+import { database } from '../utils/databaseFunctions';
+import { embed } from '../utils/embed';
+import { emojis } from '../utils/emojis';
+import { utils } from '../utils/utils';
+
+export const run = async (message: Message, client: Client, args: string[]): Promise<Message | void> => {
+    const user = (await utils.getUser(args.join(' '), client, message.guild!)) || message.author;
+    const networthEmbed = embed({
+        author: {
+            image: message.author.displayAvatarURL(),
+            name: message.author.username,
+        },
+        color: message.guild?.me?.displayHexColor,
+    });
+
+    if (user.bot) return message.channel.send(networthEmbed.setDescription(`${emojis.tickNo} Bots don't have bank accounts!`));
+
+    const networth = (await database.getProp('economy', user.id, 'networth')) || 0;
+
+    networthEmbed.setAuthor(user.username, user.displayAvatarURL());
+    message.channel.send(networthEmbed.setDescription(`${user.id === message.author.id ? 'Your' : 'Their'} networth: **$${networth.toLocaleString()}**`));
+};
+
+export const help = {
+    aliases: ['networth'],
+    name: 'Networth',
+    description: 'Sends your networth',
+    usage: 'networth <user>',
+    example: 'networth\nnetworth @Conor#0751',
+};
+
+export const config = {
+    args: 0,
+};
