9bf9d27f5bd406964a4bb142458b5b1f365e50a8
Awesome Stickz <kiran121102@gmail.com>
2020-08-24 12:12:09 +0530

    Removed reload all and reload from all args instead of args[0]

--- src/commands/reload.ts
+++ src/commands/reload.ts
@@ -4,25 +4,18 @@ import { embed } from '../utils/embed';
 import { emojis } from '../utils/emojis';
 
 export const run = async (message: Message, _client: Client, args: string[]) => {
-    if (args[0] == 'all') {
-        delete require.cache;
+    fs.access(`./commands/${args.join(' ')}.js`, fs.constants.R_OK | fs.constants.W_OK, (error: any) => {
+        if (error) {
+            const reloadEmbed = embed({ color: 'RANDOM', desc: `${emojis.tickNo} Error while reloading!. Reason: The command **${args.join(' ')}** doesn't exist!` });
 
-        const reloadEmbed = embed({ color: 'RANDOM', desc: `${emojis.tickYes} All commands have been reloaded!` });
-        message.channel.send(reloadEmbed);
-    } else {
-        fs.access(`./commands/${args[0]}.js`, fs.constants.R_OK | fs.constants.W_OK, (error: any) => {
-            if (error) {
-                const reloadEmbed = embed({ color: 'RANDOM', desc: `${emojis.tickNo} Error while reloading!. Reason: The command **${args.join(' ')}** doesn't exist!` });
+            message.channel.send(reloadEmbed);
+        } else {
+            delete require.cache[require.resolve(`./${args.join(' ')}`)];
 
-                message.channel.send(reloadEmbed);
-            } else {
-                delete require.cache[require.resolve(`./${args}`)];
-
-                const reloadEmbed = embed({ color: 'RANDOM', desc: `${emojis.tickYes} Successfully reloaded **${args[0]}**!` });
-                message.channel.send(reloadEmbed);
-            }
-        });
-    }
+            const reloadEmbed = embed({ color: 'RANDOM', desc: `${emojis.tickYes} Successfully reloaded **${args.join(' ')}**!` });
+            message.channel.send(reloadEmbed);
+        }
+    });
 };
 
 export const help = {
