ed7066463e346fffed28868cc2ca9a110e1c04e3
Awesome Stickz <kiran121102@gmail.com>
2020-08-25 12:48:38 +0530

    Added reps

---
+++ src/commands/reps.ts
@@ -0,0 +1,35 @@
+import { Client, Message } from 'discord.js';
+import { database } from '../utils/databaseFunctions';
+import { embed } from '../utils/embed';
+import { emojis } from '../utils/emojis';
+import { utils } from '../utils/utils';
+
+export const run = async (message: Message, client: Client, args: string[]): Promise<Message | void> => {
+    const user = (await utils.getUser(args.join(' '), client, message.guild!)) || message.author;
+    const repsEmbed = embed({
+        author: {
+            image: message.author.displayAvatarURL(),
+            name: message.author.username,
+        },
+        color: message.guild?.me?.displayHexColor,
+    });
+
+    if (user.bot) return message.channel.send(repsEmbed.setDescription(`${emojis.tickNo} Bots don't have reps!`));
+
+    const reps = (await database.getProp('economy', user.id, 'rep')) || 0;
+
+    repsEmbed.setAuthor(user.username, user.displayAvatarURL());
+    message.channel.send(repsEmbed.setDescription(`${user.id === message.author.id ? 'Your' : 'Their'} reps: **$${reps.toLocaleString()}**`));
+};
+
+export const help = {
+    aliases: ['reps', 'reputations'],
+    name: 'Reps',
+    description: 'View your reps',
+    usage: 'reps [user]',
+    example: 'reps\reps @Conor#0751',
+};
+
+export const config = {
+    args: 0,
+};
