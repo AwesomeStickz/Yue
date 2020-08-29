bd12b4614c57613f82c99fdbd8624655b7b8b2a5
Awesome Stickz <kiran121102@gmail.com>
2020-08-29 19:42:50 +0530

    Added slots buy

---
+++ src/commands/slots buy.ts
@@ -0,0 +1,80 @@
+import { Client, Message } from 'discord.js';
+import { database } from '../utils/databaseFunctions';
+import { embed } from '../utils/embed';
+import { emojis } from '../utils/emojis';
+
+export const run = async (message: Message, _client: Client, args: string[]): Promise<Message | void> => {
+    const slots = {
+        Worker: 75,
+        House: 150,
+        Navigator: 150,
+        Shop: 150,
+    };
+
+    const slotBuyEmbed = embed({
+        author: {
+            image: message.author.displayAvatarURL(),
+            name: message.author.username,
+        },
+        color: message.guild?.me?.displayHexColor,
+    });
+
+    const slotAmountString = args[args.length - 1].toLowerCase();
+    const slotName = slotAmountString !== 'all' && slotAmountString !== 'half' && slotAmountString !== 'quarter' && !slotAmountString.endsWith('%') && isNaN(Number(slotAmountString)) ? args.join(' ').toLowerCase() : args.slice(0, -1).join(' ').toLowerCase();
+
+    let validSlot = false;
+
+    for (const [shopSlotName, shopSlotPrice] of Object.entries(slots)) {
+        if (slotName.startsWith(shopSlotName.toLowerCase())) {
+            const balance = (await database.getProp('economy', message.author.id, 'balance')) || 0;
+
+            let amountUserInvests = 0;
+            let slotAmount = 0;
+
+            if (slotAmountString === 'all') amountUserInvests = balance;
+            else if (slotAmountString === 'half') amountUserInvests = balance / 2;
+            else if (slotAmountString === 'quarter') amountUserInvests = balance / 4;
+            else if (slotAmountString.endsWith('%')) amountUserInvests = (Number(slotAmountString.slice(0, -1)) * balance) / 100;
+            else {
+                slotAmount = Number(slotAmountString);
+                if (isNaN(slotAmount)) slotAmount = 1;
+                amountUserInvests = slotAmount * shopSlotPrice;
+            }
+
+            if (isNaN(amountUserInvests)) return message.channel.send(slotBuyEmbed.setDescription(`${emojis.tickNo} The amount of slots must be a number!`));
+
+            const numberOfSlotsToBuy = slotAmount > 0 ? Math.round(slotAmount) : Math.floor(amountUserInvests / shopSlotPrice);
+            const totalMoney = numberOfSlotsToBuy * shopSlotPrice;
+            if (totalMoney > balance) return message.channel.send(slotBuyEmbed.setDescription(`${emojis.tickNo} You don't have enough money to buy **${shopSlotName} Slot${numberOfSlotsToBuy > 1 ? 's' : ''}**`));
+
+            const userLevelData = (await database.getProp('economy', message.author.id, 'level')) || {};
+            const userLevel = userLevelData.level || 0;
+
+            // @ts-expect-error
+            const existingSlots = (await database.getProp('economy', message.author.id, `inventory.slots.${shopSlotName.toLowerCase()}s`)) || shopSlotName === 'worker' ? (!userLevel ? 2 : userLevel * 4) : !userLevel ? 1 : userLevel * 2;
+
+            await database.subtractProp('economy', message.author.id, totalMoney, 'balance');
+            // @ts-expect-error
+            await database.setProp('economy', message.author.id, existingSlots + numberOfSlotsToBuy, `inventory.slots.${shopSlotName.toLowerCase()}s`);
+
+            validSlot = true;
+            message.channel.send(slotBuyEmbed.setDescription(`${emojis.tickYes} You've successfully bought **${numberOfSlotsToBuy.toLocaleString()} ${shopSlotName} Slot${numberOfSlotsToBuy > 1 ? 's' : ''}** for **$${totalMoney.toLocaleString()}**`));
+            break;
+        }
+    }
+
+    if (!validSlot) return message.channel.send(slotBuyEmbed.setDescription(`${emojis.tickNo} That is not a valid slot!`));
+};
+
+export const help = {
+    aliases: ['slots buy'],
+    name: 'Slots Buy',
+    description: 'Buy slots from the shop for your houses, navigators, shops and workers',
+    usage: 'slot buy <slot type> [amount]',
+    example: 'shop buy flower shop\nshop buy flower worker\nshop buy mud house',
+};
+
+export const config = {
+    args: 1,
+    category: 'economy',
+};
