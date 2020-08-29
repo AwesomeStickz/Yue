39269e40c1c3d69ce2a583d44a265ea4d157f330
Awesome Stickz <kiran121102@gmail.com>
2020-08-29 19:58:54 +0530

    Added bankcapacity to PropertyNames

--- src/utils/databaseFunctions.ts
+++ src/utils/databaseFunctions.ts
@@ -2,7 +2,7 @@ import lodash from 'lodash';
 import sequelize from '../database/sequelize';
 
 type DatbaseModelNames = 'blacklist' | 'cooldown' | 'economy' | 'guildsettings' | 'yue';
-type PropertyNames = 'balance' | 'bank' | 'beg' | 'essence' | 'daily' | 'don' | 'inventory'  | 'inventory.slots'| 'level' | 'luck' | 'navigate' | 'networth' | 'owners' | 'pat' | 'prefix' | 'rentcollect' | 'rep' | 'rob' | 'robbed' | 'shopcollect' | 'streak' | 'tipcollect' | 'weekly' | 'winnings';
+type PropertyNames = 'balance' | 'bank' | 'bankcapacity' | 'beg' | 'essence' | 'daily' | 'don' | 'inventory' | 'inventory.slots' | 'level' | 'luck' | 'navigate' | 'networth' | 'owners' | 'pat' | 'prefix' | 'rentcollect' | 'rep' | 'rob' | 'robbed' | 'shopcollect' | 'streak' | 'tipcollect' | 'weekly' | 'winnings';
 
 export const database = {
     async all(model: DatbaseModelNames): Promise<object[]> {
