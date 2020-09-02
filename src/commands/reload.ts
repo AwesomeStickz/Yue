57220a1a051e6da710d663fe972194bb02718f54
Awesome Stickz <kiran121102@gmail.com>
2020-09-02 12:28:08 +0530

    Don't use RANDOM color for reload embed

--- src/commands/reload.ts
+++ src/commands/reload.ts
@@ -6,13 +6,13 @@ import { emojis } from '../utils/emojis';
 export const run = async (message: Message, _client: Client, args: string[]) => {
     fs.access(`./commands/${args.join(' ')}.js`, fs.constants.R_OK | fs.constants.W_OK, (error: any) => {
         if (error) {
-            const reloadEmbed = embed({ color: 'RANDOM', desc: `${emojis.tickNo} Error while reloading!. Reason: The command **${args.join(' ')}** doesn't exist!` });
+            const reloadEmbed = embed({ color: message.guild?.me?.displayHexColor, desc: `${emojis.tickNo} Error while reloading!. Reason: The command **${args.join(' ')}** doesn't exist!` });
 
             message.channel.send(reloadEmbed);
         } else {
             delete require.cache[require.resolve(`./${args.join(' ')}`)];
 
-            const reloadEmbed = embed({ color: 'RANDOM', desc: `${emojis.tickYes} Successfully reloaded **${args.join(' ')}**!` });
+            const reloadEmbed = embed({ color: message.guild?.me?.displayHexColor, desc: `${emojis.tickYes} Successfully reloaded **${args.join(' ')}**!` });
             message.channel.send(reloadEmbed);
         }
     });
