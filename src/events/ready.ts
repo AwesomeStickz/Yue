3ecd66bb0b2ca69a167c113e953992cbcfa433e9
Awesome Stickz <kiran121102@gmail.com>
2020-09-08 13:22:16 +0530

    Added removeBoosters() function in ready event that removes boosters if its time is over

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,5 @@
 import { Client } from 'discord.js';
+import { database } from '../utils/databaseFunctions';
 
 export const run = async (client: Client) => {
     const Ready = [
@@ -24,6 +25,21 @@ export const run = async (client: Client) => {
         client.channels.cache.sweep((channel) => channel.type !== 'text');
     };
 
+    const removeBoosters = async () => {
+        const allEconomyData: any = await database.all('economy');
+
+        for (const economyData of allEconomyData) {
+            const currentBoosters = economyData.data.boosters || [];
+            if (currentBoosters.length > 0) {
+                const newBoosters = currentBoosters.filter((booster: { name: string; endTime: number }) => booster.endTime < Date.now());
+                if (currentBoosters.length !== newBoosters.length) {
+                    newBoosters.length > 0 ? await database.setProp('economy', economyData.userid, newBoosters, 'boosters') : await database.deleteProp('economy', economyData.userid, 'boosters');
+                }
+            }
+        }
+    };
+
     setInterval(changeStatus, 20000);
     setInterval(sweepChannels, 300000);
+    setInterval(removeBoosters, 60000);
 };
