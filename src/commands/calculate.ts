01edb476dc89a834e73b3c84e6605b94bc0b1326
Awesome Stickz <kiran121102@gmail.com>
2020-09-11 16:07:50 +0530

    Added calculate

---
+++ src/commands/calculate.ts
@@ -0,0 +1,40 @@
+import { Client, Message } from 'discord.js';
+import { embed } from '../utils/embed';
+import { emojis } from '../utils/emojis';
+import { client } from '../yue';
+
+export const run = async (message: Message, _client: Client, args: string[]): Promise<Message | void> => {
+    const calculatorEmbed = embed({
+        author: {
+            image: client.user!.displayAvatarURL(),
+            name: 'Calculate',
+        },
+        color: message.guild?.me?.displayHexColor,
+    });
+
+    const expression = args.join(' ');
+    if (/[^-()\d/*+. ]/.test(expression)) return message.channel.send(calculatorEmbed.setDescription(`${emojis.tickNo} That's not a valid mathematical expression!`));
+
+    let evaluatedExpression: number;
+
+    try {
+        evaluatedExpression = eval(expression);
+    } catch (_) {
+        return message.channel.send(calculatorEmbed.setDescription(`${emojis.tickNo} That's not a valid mathematical expression!`));
+    }
+
+    message.channel.send(calculatorEmbed.setDescription(`${expression} = ${evaluatedExpression}`));
+};
+
+export const help = {
+    aliases: ['calculate', 'calc'],
+    name: 'Calculate',
+    description: 'Calculate a mathematical expression',
+    usage: 'calculate <math expression>',
+    example: 'calc (1+1)/2',
+};
+
+export const config = {
+    args: 0,
+    category: 'economy',
+};
