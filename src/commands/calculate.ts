ae0413f9dbf8e2d9b6bc46b0ae01292d695d514a
Awesome Stickz <kiran121102@gmail.com>
2020-09-16 13:47:00 +0530

    Added comma in calculate and --no-comma option to not show comma

--- src/commands/calculate.ts
+++ src/commands/calculate.ts
@@ -11,7 +11,7 @@ export const run = async (message: Message, client: Client, args: string[]): Pro
         color: message.guild?.me?.displayHexColor,
     });
 
-    const expression = args.join(' ');
+    const expression = args.filter((arg) => arg !== '--no-comma').join(' ');
     if (/[^-()\d/*+. ]/.test(expression)) return message.channel.send(calculatorEmbed.setDescription(`${emojis.tickNo} That's not a valid mathematical expression!`));
 
     let evaluatedExpression: number;
@@ -25,7 +25,7 @@ export const run = async (message: Message, client: Client, args: string[]): Pro
     calculatorEmbed.setFooter('Yue');
     calculatorEmbed.setTimestamp();
 
-    message.channel.send(calculatorEmbed.setDescription(`${expression} = ${evaluatedExpression}`));
+    message.channel.send(calculatorEmbed.setDescription(`${expression} = ${args.find((arg) => arg.toLowerCase() === '--no-comma') ? evaluatedExpression : evaluatedExpression.toLocaleString()}`));
 };
 
 export const help = {
